8d6ca5ed30dd40af8627cff0c8a7714038367481
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-30 22:43:21 +0300

    CI: replace User monkey patch with reloadable prepend

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # name: discourse-avatar-frames
 # about: Allows users to select animated CSS avatar frames
 # version: 0.1
@@ -8,16 +10,16 @@ enabled_site_setting :avatar_frames_enabled
 
 register_asset "stylesheets/common/avatar-frames.scss"
 
-after_initialize do
-  User.register_custom_field_type('avatar_frame', :string)
-  register_editable_user_custom_field(:avatar_frame)
+module ::DiscourseAvatarFrames
+  module UserExtension
+    extend ActiveSupport::Concern
 
-  # Validate frame permissions when user saves their profile
-  User.class_eval do
-    validate :validate_avatar_frame_permission
+    prepended { validate :validate_avatar_frame_permission }
+
+    private
 
     def validate_avatar_frame_permission
-      frame = self.custom_fields['avatar_frame']
+      frame = custom_fields['avatar_frame']
       return if frame.blank? || frame == 'none'
 
       config_str = SiteSetting.avatar_frames_config || ""
@@ -26,32 +28,39 @@ after_initialize do
 
       config_str.split("|").each do |conf|
         parts = conf.split(":")
-        next unless parts.length >= 3
-        
+        next if parts.length < 3
+
         id = parts[0].strip
         if id == frame
           frame_found = true
           condition = parts[2..-1].join(":").strip
-          
+
           if condition.start_with?("tl")
             req_level = condition.sub("tl", "").to_i
-            allowed = true if self.trust_level >= req_level
+            allowed = true if trust_level >= req_level
           elsif condition.start_with?("group:")
             req_group = condition.sub("group:", "").strip.downcase
-            allowed = true if self.groups.any? { |g| g.name.downcase == req_group }
+            allowed = true if groups.any? { |group| group.name.downcase == req_group }
           end
-          
+
           break
         end
       end
 
       if frame_found && !allowed
-        self.errors.add(:base, "Du hast nicht die benötigte Berechtigung für diesen Avatar Rahmen.")
+        errors.add(:base, "Du hast nicht die benötigte Berechtigung für diesen Avatar Rahmen.")
       elsif !frame_found
-        self.errors.add(:base, "Dieser Avatar Rahmen existiert nicht.")
+        errors.add(:base, "Dieser Avatar Rahmen existiert nicht.")
       end
     end
   end
+end
+
+after_initialize do
+  User.register_custom_field_type('avatar_frame', :string)
+  register_editable_user_custom_field(:avatar_frame)
+
+  reloadable_patch { ::User.prepend(DiscourseAvatarFrames::UserExtension) }
 
   # Make it public so the frontend can read it everywhere
   allow_public_user_custom_field(:avatar_frame)
